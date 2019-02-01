d2f1ae63099cecfa62b90b357976de81cb915e5b
muhammad Rusdi <muhamamdrusdi@icloud.com>
2019-02-01 16:40:40 +0800

    edit

--- src/components/Highlight/index.jsx
+++ src/components/Highlight/index.jsx
@@ -7,7 +7,7 @@ export const Pre = styled.pre`
   text-align: left;
   margin: 1em 0;
   padding: 0.5em;
-  background-color: none !important;
+  background-color: transparent !important;
   & .token-line {
     line-height: 1.3em;
     height: 1.3em;
